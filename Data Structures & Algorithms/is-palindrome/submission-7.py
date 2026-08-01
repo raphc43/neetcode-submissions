652862f36fc6f3250affa026a7a8eb957912607a
Raphael Qureshi <darkslider345@gmail.com>
2026-08-01 22:45:23 +0500

    Add: is-palindrome - submission-7

---
+++ Data Structures & Algorithms/is-palindrome/submission-7.py
@@ -0,0 +1,29 @@
+import re
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        i = 0
+        reverse_i = len(s) - 1
+
+
+        while i <= reverse_i:   
+            print(f"FIRST {i}")
+            print(f"SECOND {reverse_i}")
+
+            if s[i].isalnum() == False:
+                i += 1
+                continue
+
+            if s[reverse_i].isalnum() == False:
+                reverse_i -= 1
+                continue
+
+            # If either side mismatches then return False
+            if s[i].lower() != s[reverse_i].lower():
+                print("FALSE")
+                return False
+
+            i += 1
+            reverse_i -= 1
+
+        return True
